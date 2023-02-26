f82d15ba0cf66c66999c4811a13c6bfc058a20f5
ErickGLopes <99217380+ErickGLopes@users.noreply.github.com>
2023-02-26 17:34:20 -0300

    Update jfw.py

--- jfw.py
+++ jfw.py
@@ -9,26 +9,26 @@ class Jfw:
     def __init__(self, porta, baud, firmware_path):
         self._port = porta
         self._baud = baud
-        self._serial = serial.Serial(_port, _baud)
-        self._esp = esptool.ESP32Serial(_port, baud=_baud)
+        self._serial = serial.Serial(porta, baud)
+        self._esp = esptool.ESP32Serial(porta, baud=baud)
         self._firmware_path = firmware_path 
        
-        @property
+ 
         def flash(self):
             self._esp.baud = 460800
             self._esp.before_connect()
             self._esp.connect()
-            self._esp.flash_file(_firmware_path, 0x1000)
+            self._esp.flash_file(self.firmware_path, 0x1000)
             self._esp.hard_reset()
             self._serial.close()
             
-            
+           
+myesp = Jfw('COM6', 115200, 'C:\\Users\\erick\\Downloads\\esp32-20220618-v1.19.1.bin')
+myesp.flash()
+           
 '''
 Como deve funcionar:
-
 meu_esp = Jfw('\dev\ttyUSB0', 115200, '\\User\\Desktop\\firmware.bin')
 meu_esp.flash
-
 att: não testado
 '''
- 
